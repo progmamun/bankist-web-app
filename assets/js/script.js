2256dc514368f25bf11383865b99322ebb4b7c71
progmamun <mamun333089@gmail.com>
2022-02-04 12:43:02 +0600

    filter-method

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,3 +112,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //     console.log(`You withdrew ${Math.abs(movement)}`);
 //   }
 // });
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+const withdrawals = movements.filter(mov => mov < 0);
