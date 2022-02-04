87a9fbe5829d7301e102c09611fa6a8ff19a94da
progmamun <mamun333089@gmail.com>
2022-02-04 12:20:50 +0600

    create-use-name

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,6 +77,19 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
