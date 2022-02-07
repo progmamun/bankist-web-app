6fd669167721977b8fcc2def8ad39790185a29d5
progmamun <mamun333089@gmail.com>
2022-02-07 16:40:48 +0600

    reminder-operator

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,3 +257,12 @@ console.log(Number('23'));
 console.log(+'23');
 
 // Parsing
+
+// reminder operator (%)
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
